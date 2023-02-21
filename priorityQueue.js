ba1199073ee1802e4673573b752e21c3cc07b809
Théo Louvel <66867063+flowsn4ke@users.noreply.github.com>
2023-02-21 18:24:21 +0100

    Update PQ to accept custom comparator

--- priorityQueue.js
+++ priorityQueue.js
@@ -1,55 +1,80 @@
-class Node {
-    constructor(val, priority) {
-        this.val = val
-        this.priority = priority
-    }
-}
-
 class PriorityQueue {
-    constructor() {
-        this.values = []
+  constructor(comparator) {
+    this.elements = []
+    this.comparator = comparator
+  }
+  idx_is_valid(idx) {
+    return idx < this.elements.length && idx >= 0
+  }
+  return_elements() {
+    return this.elements
+  }
+  swap(i, j) {
+    const temp = this.elements[i]
+    this.elements[i] = this.elements[j]
+    this.elements[j] = temp
+  }
+  find_parent_idx(idx) {
+    return (idx - (idx % 2 === 0 ? 2 : 1)) / 2
+  }
+  find_children_idx(idx) {
+    let child_1_idx = (2 * idx + 1)
+    let child_2_idx = (2 * idx + 2)
+
+    child_1_idx = this.idx_is_valid(child_1_idx) ? child_1_idx : null
+    child_2_idx = this.idx_is_valid(child_2_idx) ? child_2_idx : null
+
+    return [child_1_idx, child_2_idx]
+  }
+  bubble_up(idx) {
+    if (idx < 1) return
+
+    const element = this.elements[idx]
+    const parent_idx = this.find_parent_idx(idx)
+    const parent_element = this.elements[parent_idx]
+    const should_bubble_up = this.comparator(element, parent_element)
+
+    if (should_bubble_up) {
+      this.swap(idx, parent_idx)
+      this.bubble_up(parent_idx)
     }
-    swap(i, j) {
-        const temp = this.values[i]
-        this.values[i] = this.values[j]
-        this.values[j] = temp
+  }
+  bubble_down(idx) {
+    const element = this.elements[idx]
+
+    const children_idx = this.find_children_idx(idx)
+    const child_1_idx = children_idx[0]
+    const child_2_idx = children_idx[1]
+
+    const child_1 = child_1_idx ? this.elements[child_1_idx] : null
+    const child_2 = child_2_idx ? this.elements[child_2_idx] : null
+
+    if (!child_1 && !child_2) return
+
+    const first_child_idx = child_1 !== null && child_2 !== null
+      ? this.comparator(child_1, child_2)
+        ? child_1_idx
+        : child_2_idx
+      : (child_2_idx || child_1_idx)
+
+    const first_child_should_bubble_up = this.comparator(this.elements[first_child_idx], element)
+
+    if (first_child_should_bubble_up) {
+      this.swap(idx, first_child_idx)
+      this.bubble_down(first_child_idx)
     }
-    findParent(i) {
-        return (i - (i % 2 === 0 ? 2 : 1)) / 2
-    }
-    findChildren(i) {
-        return [(2 * i + 1), (2 * i + 2)]
-    }
-    enqueue(val, priority) {
-        const new_node = new Node(val, priority)
-        this.values.push(new_node)
-        let currentIndex = this.values.length - 1
-        const bubbleUp = (i) => {
-            if (i < 1) return
-            let parentIndex = this.findParent(i)
-            if (priority < this.values[parentIndex].priority) {
-                this.swap(i, parentIndex)
-                bubbleUp(parentIndex)
-            }
-        }
-        bubbleUp(currentIndex)
-        return this.values.length
-    }
-    dequeue() {
-        this.swap(0, this.values.length - 1)
-        const old_root = this.values.pop()
-        const bubbleDown = (i) => {
-            const children = this.findChildren(i)
-            if (children[0] > this.length - 1 && children[1] > this.length - 1) return
-            if (this.values[children[0]]?.priority < this.values[children[1]]?.priority) {
-                this.swap(i, children[0])
-                bubbleDown(children[0])
-            } else if (this.values[children[0]]?.priority > this.values[children[1]]?.priority) {
-                this.swap(i, children[1])
-                bubbleDown(children[1])
-            } else return
-        }
-        bubbleDown(0)
-        return old_root
-    }
+  }
+  enqueue(element) {
+    this.elements.push(element)
+    let inserted_idx = this.elements.length - 1
+    this.bubble_up(inserted_idx)
+    return this.elements.length
+  }
+  dequeue() {
+    if (this.elements.length === 0) return null
+    this.swap(0, this.elements.length - 1)
+    const element = this.elements.pop()
+    this.bubble_down(0)
+    return element || null
+  }
 }
